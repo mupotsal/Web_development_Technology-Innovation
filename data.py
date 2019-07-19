1989d63a553fc135e1d855023b63e52f3daafe67
mupotsal <46546491+mupotsal@users.noreply.github.com>
2019-07-18 23:26:38 -0400

    first Push finished the navbar

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':'1',
+            'title':'Article Opne',
+            'body':'Content of the first article',
+            'author':'Liberty Mupotsa',
+            'create date':'07-16-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article two',
+            'body': 'Content of the second article',
+            'author': 'Walter Mupotsa',
+        },
+        {
+            'id': 3,
+            'title': 'Article 3',
+            'body': 'Content of the third article',
+            'author': 'Liberty Mupotsa',
+            'create date': '07-16-2019'
+        }
+    ]
+    return articles
+
--- venv/webdev.py
+++ venv/webdev.py
@@ -8,23 +8,37 @@
 
 
 
-from flask import Flask
+from flask import Flask, render_template,flash,url_for, sessions, logging
+
 
 from flask import render_template
+from data import Articles
+
 
 app = Flask(__name__)
 
 # @ is the decorator , its a way to wrap a function and wrap its behavior
-
-
+Articles = Articles()
 @app.route('/')
 def index():
+
 
     return render_template("index.html")
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+@app.route('/articles')
+def articles():
+    return render_template("articles.html", articles = Articles)
+
+
+@app.route('/articles/<string:id>/')
+def article(id):
+    return render_template("article.html", id= id )
+
 
 # @app.route('/tuna') # You can create multiple pages but there is need of putting the decorator before.
 # def tuna():
@@ -34,5 +48,6 @@ def about():
 # def profile(username):
 #     return '<h2> Hey there %s </h2>' % username
 
-if __name__ =="__main__":
+if __name__ =="__main__"\
+        :
     app.run(debug=True)
